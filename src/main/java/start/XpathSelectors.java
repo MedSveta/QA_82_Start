ee63738e9072bf5a1fd4a3488f070c2ba075b126
MedSveta <sveta1978medved@gmail.com>
2026-05-20 15:28:29 +0300

    XpathSelectors

--- src/main/java/start/XpathSelectors.java
+++ src/main/java/start/XpathSelectors.java
@@ -11,6 +11,22 @@ public class XpathSelectors {
     WebDriver driver = new ChromeDriver();
 
     @Test
+    public void practiceFormTest(){
+        driver.get("https://demoqa.com/");
+        driver.manage().window().maximize();
+        hideFooter();
+        WebElement divElements = driver.findElement
+                (By.xpath("//div[@class='category-cards']/a[2]"));
+        divElements.click();
+        WebElement spanPracticeForm = driver.findElement
+                (By.xpath("//span[text()='Practice Form']"));
+        spanPracticeForm.click();
+
+        pause(3000);
+        driver.quit();
+    }
+
+    @Test
     public void textBoxXpathTest(){
         driver.get("https://demoqa.com/text-box");
         driver.manage().window().maximize();
@@ -25,7 +41,24 @@ public class XpathSelectors {
         WebElement inputFullName = driver.findElement
                 (By.xpath("//input[@placeholder='Full Name']"));
         inputFullName.sendKeys("Monkey");
-
+        //  //input[@id='userEmail']
+        //  //*[@id='userEmail']
+        //  //form/div[2]/div[2]/input
+        WebElement inputEmail = driver.findElement
+                (By.xpath("//form/div[2]/div[2]/input"));
+        inputEmail.sendKeys("monkey123@gmail.com");
+        // //*[text()='Current Address']/../..//textarea
+        WebElement textareaCurAddress = driver.findElement
+                (By.xpath("//*[text()='Current Address']/../..//textarea"));
+        textareaCurAddress.sendKeys("monkey street 1");
+        WebElement textareaPerAddress = driver.findElement
+                (By.xpath("//div[@id='permanentAddress-wrapper']/div[2]/textarea"));
+        textareaPerAddress.sendKeys("monkey avenue 4");
+        WebElement btnSubmit = driver.findElement(By.xpath("//button[@id='submit']"));
+        btnSubmit.click();
+        WebElement output = driver.findElement(By.xpath("//*[@id='output']"));
+        System.out.println(output.getText());
+        pause(3000);
         driver.quit();
     }
 
