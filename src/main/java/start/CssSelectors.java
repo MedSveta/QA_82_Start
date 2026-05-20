668c66f4b49339e1702be2b9dcc9113936047db8
MedSveta <sveta1978medved@gmail.com>
2026-05-20 12:01:15 +0300

    Start1

--- src/main/java/start/CssSelectors.java
+++ src/main/java/start/CssSelectors.java
@@ -49,12 +49,61 @@ public class CssSelectors {
         WebElement textBox1 = driver.findElement(By
                 .cssSelector("#item-0"));
         textBox1.click();
-        pause(5000);
+        pause(3000);
         driver.navigate().back();
         WebElement textBox2 = driver.findElement(By
                 .id("item-0"));
         textBox2.click();
-        pause(5000);
+        pause(3000);
+        driver.navigate().back();
+        WebElement textBox3 = driver.findElement(By
+                .cssSelector("li#item-0.btn.btn-light"));
+        textBox3.click();
+        pause(3000);
+       // WebElement textBoxFull = driver.findElement(By.cssSelector("div[class*='-field']")); //contains
+       // WebElement textBoxFull = driver.findElement(By.cssSelector("div[class^='text-']"));   // start with
+        WebElement textBoxFull = driver.findElement(By.cssSelector("div[class$='-container']"));  //end with
+        System.out.println(textBoxFull.getTagName());
+        pause(3000);
+        driver.quit();
+    }
+
+    @Test
+    public void selectorsCssChildTest() {
+        driver.get("https://demoqa.com/");
+        pause(3000);
+        driver.manage().window().maximize();
+        pause(3000);
+        hideFooter();
+        WebElement divElements = driver.findElement
+                (By.cssSelector("div[class='card-body']"));
+        divElements.click();
+        pause(3000);
+        driver.navigate().back();
+        pause(3000);
+        WebElement divAlerts = driver.findElement
+                (By.cssSelector("div.category-cards a:nth-child(3)"));
+        divAlerts.click();
+        pause(3000);
+        driver.navigate().back();
+        pause(3000);
+        WebElement divWidgets = driver.findElement
+                (By.cssSelector("div.category-cards a:nth-child(4)"));
+        divWidgets.click();
+        pause(3000);
+        driver.navigate().back();
+        pause(3000);
+        WebElement divElements1 = driver.findElement
+                (By.cssSelector("div.category-cards a:first-child"));
+        divElements1.click();
+        pause(3000);
+        driver.navigate().back();
+        scrollActions();
+        pause(3000);
+        WebElement divBookStore = driver.findElement
+                (By.cssSelector("div.category-cards a:last-child"));
+        divBookStore.click();
+        pause(3000);
         driver.quit();
     }
 
@@ -67,7 +116,7 @@ public class CssSelectors {
     public void scrollActions(){
         Actions actions = new Actions(driver);
         for (int i = 0; i < 10; i++) {
-            actions.scrollByAmount(0, 0).perform();
+            actions.scrollByAmount(0, 500).perform();
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
