4c0b5d9c62fdebcd5a13774da458b915dd4ecc99
MedSveta <sveta1978medved@gmail.com>
2026-05-26 12:29:28 +0300

    PracticeFormPage Test

--- src/main/java/pages/PracticeFormPage.java
+++ src/main/java/pages/PracticeFormPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import dto.Student;
 import enums.Gender;
+import enums.Hobbies;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -9,6 +10,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+import java.util.List;
 
 public class PracticeFormPage extends BasePage {
     public PracticeFormPage(WebDriver driver) {
@@ -30,6 +33,15 @@ public class PracticeFormPage extends BasePage {
     WebElement inputDateOfBirth;
     @FindBy(css = ".subjects-auto-complete__input")
     WebElement inputSubjects;
+    @FindBy(id = "react-select-3-input")
+    WebElement inputState;
+    @FindBy(id = "react-select-4-input")
+    WebElement inputCity;
+    @FindBy(xpath = "//button[text()='Submit']")
+    WebElement btnSubmit;
+    @FindBy(id = "example-modal-sizes-title-lg")
+    WebElement modalMessage;
+
 
     public void typePracticeForm(Student student) {
         inputFirstName.sendKeys(student.getFirstName());
@@ -39,16 +51,43 @@ public class PracticeFormPage extends BasePage {
         inputMobile.sendKeys(student.getMobile());
         typeDateOfBirth(student.getDateOfBirth());
         typeSubjects(student.getSubjects());
+        typeHobbies(student.getHobbies());
         textareaAddress.sendKeys(student.getAddress());
+        typeStateCity(student.getState(), student.getCity());
+        scrollActions();
+        btnSubmit.click();
     }
 
-    private void typeSubjects(String subjects){
-       inputSubjects.click();
-       String[] strArr = subjects.trim().split(",");
-       for (String s: strArr){
-           inputSubjects.sendKeys(s);
-           inputSubjects.sendKeys(Keys.ENTER);
-       }
+
+    public boolean isTextInModalMessageValid(String text){
+        return isTextInElementPresent(modalMessage, text);
+    }
+
+    private void typeStateCity(String state, String city) {
+        inputState.sendKeys(state);
+        inputState.sendKeys(Keys.ENTER);
+
+        inputCity.sendKeys(city);
+        inputCity.sendKeys(Keys.ENTER);
+    }
+
+    private void typeHobbies(List<Hobbies> hobbies) {
+        for (Hobbies h : hobbies) {
+            switch (h) {
+                case SPORTS -> driver.findElement(By.id(h.getLocator())).click();
+                case MUSIC -> driver.findElement(By.id(h.getLocator())).click();
+                case READING -> driver.findElement(By.id(h.getLocator())).click();
+            }
+        }
+    }
+
+    private void typeSubjects(String subjects) {
+        inputSubjects.click();
+        String[] strArr = subjects.trim().split(",");
+        for (String s : strArr) {
+            inputSubjects.sendKeys(s);
+            inputSubjects.sendKeys(Keys.ENTER);
+        }
     }
 
     private void typeGender(Gender gender) {
@@ -60,7 +99,7 @@ public class PracticeFormPage extends BasePage {
         String operationSystem = System.getProperty("os.name");
         System.out.println(operationSystem);
         if (operationSystem.startsWith("Win"))
-            inputDateOfBirth.sendKeys(Keys.chord(Keys.CONTROL,"a"));
+            inputDateOfBirth.sendKeys(Keys.chord(Keys.CONTROL, "a"));
         if (operationSystem.startsWith("Mac"))
             inputDateOfBirth.sendKeys(Keys.chord(Keys.COMMAND, "a"));
         inputDateOfBirth.sendKeys(dateOfBirth);
