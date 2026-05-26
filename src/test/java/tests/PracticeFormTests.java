38d17b043f3efeb7c5544aee96068fc55a22c30a
MedSveta <sveta1978medved@gmail.com>
2026-05-26 14:36:24 +0300

    PracticeFormPage Test

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -5,8 +5,10 @@ import enums.Gender;
 import enums.Hobbies;
 import enums.StateCity;
 import manager.AppManager;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.FormsPage;
 import pages.HomePage;
 import pages.PracticeFormPage;
@@ -15,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeFormTests extends AppManager {
+    SoftAssert softAssert = new SoftAssert();
 
     // Thanks for submitting the form
     @Test
@@ -33,7 +36,44 @@ public class PracticeFormTests extends AppManager {
         new FormsPage(getDriver()).clickBtnPracticeForm();
         PracticeFormPage practiceFormPage = new PracticeFormPage(getDriver());
         practiceFormPage.typePracticeForm(student);
+//        Assert.assertTrue(practiceFormPage.isTextInModalMessageValid
+//                ("Wrong"));
+//        System.out.println("program is working");
         Assert.assertTrue(practiceFormPage.isTextInModalMessageValid
                 ("Thanks for submitting the form"));
+    }
+
+    @Test
+    public void practiceFormPositiveWithSoftAssertTest() {
+        List<Hobbies> hobbies = new ArrayList<>();
+        hobbies.add(Hobbies.READING);
+        hobbies.add(Hobbies.SPORTS);
+        Student student = new Student("Maksim",
+                "Maksimov", "maksimov325@gmail.com",
+                Gender.MALE, "1234567890",
+                "23 Mar 2001", " Maths,Chemistry,English ",
+                hobbies, "", "address 1",
+                StateCity.UTTAR_PRADESH.getState(), StateCity.UTTAR_PRADESH.getCity()[1]);
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnForms();
+        new FormsPage(getDriver()).clickBtnPracticeForm();
+        PracticeFormPage practiceFormPage = new PracticeFormPage(getDriver());
+        practiceFormPage.typePracticeForm(student);
+//        softAssert.assertTrue(practiceFormPage.isTextInModalMessageValid
+//                ("Wrong"), "validate wrong modal message");
+      //  System.out.println("program is working");
+        softAssert.assertTrue(practiceFormPage.isTextInModalMessageValid
+                ("Thanks for submitting the form"),
+                "validate right modal message");
+        softAssert.assertTrue(getDriver().findElement
+                (By.xpath("//tbody/tr/td[2]")).getText().
+                contains(student.getFirstName()), "validate firstName");
+        softAssert.assertTrue(getDriver().findElement
+                        (By.xpath("//tbody/tr/td[2]")).getText().
+                contains(student.getLastName()), "validate lastName");
+        softAssert.assertTrue(getDriver().findElement
+                        (By.xpath("//tbody/tr[2]/td[2]")).getText().
+                contains(student.getEmail()), "validate Email");
+        softAssert.assertAll();
     }
 }
